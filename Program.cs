ad2f30cd7f9570a7d2205484e4dff84b185c5451
Sergey <shadowmare@mail.ru>
2022-12-16 15:50:13 +0300

    Iteration with classes

--- Program.cs
+++ Program.cs
@@ -1,21 +1,37 @@
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 HttpClient client = new();
 const string url = "https://pres.tsrealty.ru/testing.php";
 
-
 var tsRealtyJson = await client.GetStringAsync(url);
-JObject parsedJson = JObject.Parse(tsRealtyJson);
-JObject fields = (JObject)parsedJson["result"]!["fields"]!;
-
-var query = fields.Properties().Select(n => new { Name = n.Name,
-                                                  Type = n.Children().Select(t => t["type"]!).First().Value<string>(),
-                                                  Title = n.Children().Select(t => t["title"]!).First().Value<string>() });
-
-foreach (var data in query)
+var parsedJson = JsonConvert.DeserializeObject<Root>(tsRealtyJson);
+foreach (var field in parsedJson.Result.Fields)
 {
-    if (data.Type == "enumeration")
+    async Task dataBaseEmulation()
     {
-        Console.WriteLine($"Поле: {data.Name} имеет название: {data.Title} и является полем типа enumeration");
+        Console.WriteLine($"Поле {field.Key} добавлено в базу данных");
     }
+    
+    if (field.Value.Type == "enumeration")
+    {
+        Console.WriteLine($"Поле: {field.Key}, имеет название: {field.Value.Title} и является полем типа {field.Value.Type}");
+        dataBaseEmulation();
+    }
+}
+
+public class Root
+{
+    public Result? Result { get; set; }
+}
+
+public class Result
+{
+    public Dictionary<string, Values>? Fields { get; set; }
+}
+
+public class Values
+{
+    public string? Type { get; set; }
+    public string? Title { get; set; }
 }
